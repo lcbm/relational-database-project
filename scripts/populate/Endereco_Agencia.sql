13327a528c03495c4f10ef667f6cc639d7557530
Leonardo Melo <lcbm@cesar.school>
2019-11-05 13:52:07 -0300

    Add scripts to populate the database

---
+++ scripts/populate/Endereco_Agencia.sql
@@ -0,0 +1,26 @@
+#
+# TABLE STRUCTURE FOR: Endereco_Agencia
+#
+
+USE comportamento_digital;
+
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (4060, 81207, 96263, '00461', '7231');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (963, 30493, 27203, '02133-64', '2552');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (383, 42884, 67725, '05598', '541');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (874, 86947, 73431, '14183-35', '3123');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (6662, 71272, 40703, '16403', '3721');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (5509, 43597, 8344, '18978', '8619');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (5398, 1035, 38365, '22642-61', '4617');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (3210, 57075, 57209, '26396', '7624');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (5299, 84909, 83867, '28705', '2749');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (4992, 96001, 97917, '33623', '4215');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (5051, 75643, 891, '39918', '9351');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (294, 48298, 55716, '52717-92', '2153');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (2594, 32746, 99409, '58609-35', '8913');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (9796, 61611, 31268, '60280', '5582');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (1454, 76942, 54215, '60695', '6679');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (2197, 32266, 7145, '78541-56', '9278');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (4927, 24976, 20600, '78893', '4669');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (2991, 86353, 18383, '88943-67', '2205');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (6840, 47549, 57890, '89462', '4908');
+INSERT INTO `Endereco_Agencia` (`numero`, `codigo_banco`, `codigo_agencia`, `cep`, `complemento`) VALUES (9561, 29946, 33443, '90180', '9613');
